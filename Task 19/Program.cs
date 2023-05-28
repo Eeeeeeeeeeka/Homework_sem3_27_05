ff8ea97f33a7813b11d8bb12cd0ea96f3c512d11
Elena_Kalegina <ea.kalegina@yandex.ru>
2023-05-28 12:39:53 +0500

    Task 19 done

---
+++ Task 19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19 
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 9999 || n > 100000)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+string number = n.ToString();
+if (number[0] == number[4] && number[1] == number[3])
+Console.Write($"Число {n} является палиндромом");
+else
+    Console.Write($"Число {n} не является палиндромом");
---
+++ Task additional/Program.cs
@@ -0,0 +1,2 @@
+﻿// https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=5&id_topic=113&id_problem=695
+
